7347fb1d3edcfeb835802ec05ac1ac9871abc2b3
alexandraBouillon <alexandra.bouillon@gmail.com>
2021-06-03 13:36:46 -0700

    built the tree

--- tree.py
+++ tree.py
@@ -14,20 +14,41 @@ class Node:
 # input needed
 
     @property
-    def parent(self, node):
-        self._parent = node
-        self.add_children(node)
-
-    @parent_getter
     def parent(self):
         return self._parent
+
+    @parent.setter
+    def parent(self, node):
+        if self._parent is node:
+            return
+        if self._parent is not None:
+            self._parent.remove_child(self)
+        # then self.parentremove(self)
+        # self.remove_child(node)
+        self._parent = node
+        if node is not None:
+            node.add_child(self)
+
+
 # input unneeded
 
-    def add_children(self, node):
-        if not node in self._children:
+    def add_child(self, node):
+        if node not in self._children:
             self._children.append(node)
-            node._parent = self
+            node.parent = self
 
     def remove_child(self, node):
-        self._children.remove(node)
-        node._parent = None
+        if node in self._children:
+            self._children.remove(node)
+            node.parent = None
+
+
+node1 = Node("root1")
+node2 = Node("root2")
+node3 = Node("root3")
+
+node3.parent = node1
+node3.parent = node2
+
+print(node1.children)
+print(node2.children)
