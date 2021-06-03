c147b0aa35d453ad06aee948be3d088b7b15f972
alexandraBouillon <alexandra.bouillon@gmail.com>
2021-06-03 16:26:21 -0700

    working on move postion

---
+++ path_finder.py
@@ -0,0 +1,28 @@
+from tree import Node
+
+
+class KnightPathFinder:
+    def __init__(self, start_pos=()):
+        # Creating an instance variable, self._root
+
+        # self.postion = Node(start_position)
+        self._root = Node(start_pos)
+        self._considered_positions = set(start_pos)
+
+# (x,y) = pos
+# (x,y) = possible_move
+    def get_valid_moves(self, pos):
+        possible_moves = [
+            (1, 2),
+            (1, -2),
+            (2, 1),
+            (-2, 1),
+            (-1, 2),
+            (-1, -2),
+            (-1, -2),
+            (-2, -1),
+        ]
+        return possible_moves
+
+
+finder = KnightPathFinder((0, 0))
--- path_finders.py
+++
@@ -1,9 +0,0 @@
-from tree import Node
-
-
-class KnightPathFinder:
-    def __init__(self, paths=()):
-        self.paths = paths
-
-
-finder = KnightPathFinder((0, 0))
